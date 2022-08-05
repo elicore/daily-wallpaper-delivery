f1feffd48efab580526a50c93d593be9f16d32c0
Eli Cohen <eli.cohen@redis.com>
2022-08-05 09:24:06 -0400

    Send images as document

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ def bot_send_photo(url: str, bot_token: str, chat_id: str, caption: str):
     print(message.json())
 
 
+def bot_send_document(url: str, bot_token: str, chat_id: str, caption: str):
+    message = requests.post(
+        f'https://api.telegram.org/bot{bot_token}/sendDocument',
+        json={'chat_id': chat_id, 'document': url, 'caption': caption, 'parse_mode': 'html'})
+
+    print(message.json())
+
+
 
 def main(bot_token: str, chat_id: str):
     # send bing photo
     (url, caption) = bing.get_wallpaper_details()
-    bot_send_photo(url, bot_token, chat_id, caption)
+    # bot_send_photo(url, bot_token, chat_id, caption)
+    bot_send_document(url, bot_token, chat_id, caption)
 
     # send spotlight photo
     for url, caption in spotlight.get_wallpaper_details():
-        bot_send_photo(url, bot_token, chat_id, caption)
+        # bot_send_photo(url, bot_token, chat_id, caption)
+        bot_send_document(url, bot_token, chat_id, caption)
 
 
 
