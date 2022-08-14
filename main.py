c0394989d770aa7342bbadd13d7ac4f0c7b348f5
Eli Cohen <amihai.cohen+github@gmail.com>
2022-08-13 22:53:05 -0400

    formatting

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import sys
 from providers import bing, spotlight
 
 
-
 def bot_send_photo(url: str, bot_token: str, chat_id: str, caption: str):
     message = requests.post(
         f'https://api.telegram.org/bot{bot_token}/sendPhoto',
-        json={'chat_id': chat_id, 'photo': url, 'caption': caption, 'parse_mode': 'html'})
+        json={
+            'chat_id': chat_id,
+            'photo': url,
+            'caption': caption,
+            'parse_mode': 'html'
+        })
 
     print(message.json())
 
@@ -16,10 +20,14 @@ def bot_send_photo(url: str, bot_token: str, chat_id: str, caption: str):
 def bot_send_document(url: str, bot_token: str, chat_id: str, caption: str):
     message = requests.post(
         f'https://api.telegram.org/bot{bot_token}/sendDocument',
-        json={'chat_id': chat_id, 'document': url, 'caption': caption, 'parse_mode': 'html'})
+        json={
+            'chat_id': chat_id,
+            'document': url,
+            'caption': caption,
+            'parse_mode': 'html'
+        })
 
     print(message.json())
-
 
 
 def main(bot_token: str, chat_id: str):
@@ -32,7 +40,6 @@ def main(bot_token: str, chat_id: str):
     for url, caption in spotlight.get_wallpaper_details():
         # bot_send_photo(url, bot_token, chat_id, caption)
         bot_send_document(url, bot_token, chat_id, caption)
-
 
 
 if __name__ == '__main__':
